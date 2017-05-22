5bd8ee4bbf025adc17a57bb57406b78602cbd9ac
Reece Robinson <Reece@TheRobinsons.gen.nz>
2017-05-22 20:22:54 +1200

    Refactor for arbitrary mel stride relative to mel width.

--- notebooks/Audio Aquisition/spectrogram.py
+++ notebooks/Audio Aquisition/spectrogram.py
@@ -6,11 +6,15 @@ import numpy as np
 import shutil
 import librosa
 import time
+import sounddevice as sd
+import sys
 
 class RingBuffer:
-    """RingBuffer class"""
+    """
+    This class provides a configurable length ndarray based byte ring buffer for raw audio data.
+    """
 
-    def __init__(self, chunk_length, num_chunks, bytes_per_frame, sample_rate):
+    def __init__(self, chunk_length, num_chunks, sample_rate):
         self.chunk_length = chunk_length
         self.ring_length = num_chunks*chunk_length
         self.ring_buffer = np.zeros(self.ring_length, dtype=np.float32)
@@ -42,6 +46,9 @@ class RingBuffer:
 
 
 class ResultRingBuffer:
+    """
+    This class provides a configurable length ndarray based ring buffer for mel spectrogram calculation results.
+    """
     def __init__(self, size, item_size):
         self.size = size
         self.ring_buffer = np.zeros((size,item_size))
@@ -57,7 +64,9 @@ class ResultRingBuffer:
 
     def save_ring_buffer(self, filename="mel_ring_buffer.txt"):
         np.savetxt(filename,self.ring_buffer[::-1])
-
+#
+# Main program logic
+#
 
 usage_line = ' press <enter> to quit, +<enter> or -<enter> to change scaling '
 frames_log = np.array([])
@@ -96,15 +105,33 @@ low, high = args.range
 if high <= low:
     parser.error('HIGH must be greater than LOW')
 
-ring = RingBuffer(1200, 500, 1, 48000)
+#
+# Capture parameters
+#
+try:
+    rec_sample_rate = sd.query_devices(args.device, 'input')['default_samplerate']
+except Exception as e:
+    parser.exit(type(e).__name__ + ': ' + str(e))
 
+target_window_length = 3.5 # seconds
 mel_resample_rate = 16000
 num_mels = 40
 mel_stride = 0.01
 mel_width = 0.025
-target_window_length = 3.5 # seconds
 mel_ring_size = int((target_window_length / mel_stride) + 1)
+
 mel_ring = ResultRingBuffer(mel_ring_size, num_mels)
+
+audio_ring_size = int(rec_sample_rate*mel_width)
+audio_chunk_count = int(1/mel_width)*10 # ten seconds
+
+audio_ring = RingBuffer(audio_ring_size, audio_chunk_count, rec_sample_rate)
+
+#
+# Processing loop attributes
+#
+stride_frac_residual = 0
+mel_stride_bytes = mel_stride * rec_sample_rate
 
 # Create a nice output gradient using ANSI escape sequences.
 # Stolen from https://gist.github.com/maurisvh/df919538bcef391bc89f
@@ -119,20 +146,21 @@ for bg, fg in zip(colors, colors[1:]):
             gradient.append('\x1b[{};{}m{}'.format(fg, bg + 10, char))
 
 try:
-    import sounddevice as sd
 
     if args.list_devices:
         print(sd.query_devices())
         parser.exit(0)
 
-    samplerate = sd.query_devices(args.device, 'input')['default_samplerate']
-
     delta_f = (high - low) / (args.columns - 1)
-    fftsize = math.ceil(samplerate / delta_f)
+    fftsize = math.ceil(rec_sample_rate / delta_f)
     low_bin = math.floor(low / delta_f)
 
+    def run_inferencing():
+        # run the trained RNN model for keyword spotting
+        pass
+
     def calc_mel_for_frame(x):
-        x_16k = librosa.resample(args.gain * x,samplerate, mel_resample_rate)
+        x_16k = librosa.resample(args.gain * x,rec_sample_rate, mel_resample_rate)
         frame = x_16k * np.hamming(len(x_16k))
         frame_magnitude = np.abs(np.fft.rfft(frame, n=fftsize))
         frame_power = ( 1 / fftsize ) * (frame_magnitude ** 2 )
@@ -140,9 +168,9 @@ try:
         return np.dot(mel_basis, frame_power)
 
     def calc_mel_for_window(size):
-        x = ring.read_window(size)
-        x_16k = librosa.resample(x,samplerate, mel_resample_rate)
-        S = librosa.feature.melspectrogram(x_16k, sr=mel_resample_rate, n_mels=num_mels, fmin=low, fmax=high, hop_length=int(samplerate/mel_stride))
+        x = audio_ring.read_window(size)
+        x_16k = librosa.resample(x,rec_sample_rate, mel_resample_rate)
+        S = librosa.feature.melspectrogram(x_16k, sr=mel_resample_rate, n_mels=num_mels, fmin=low, fmax=high, hop_length=int(rec_sample_rate/mel_stride))
 
         # Convert to log scale (dB). We'll use the peak power as reference.
         log_S = librosa.logamplitude(S, ref_power=np.max)
@@ -154,31 +182,35 @@ try:
 
     def callback(indata, frames, time, status):
         global frames_log
+        global stride_frac_residual
+
         if status: 
             text = ' ' + str(status) + ' '
             print('\x1b[34;40m', text.center(args.columns, '#'),
                   '\x1b[0m', sep='')
         if any(indata):
             
-            ring.add_chunk(indata)
+            audio_ring.add_chunk(indata)
 
-            if( ring.frames > 0 and ring.frames%2 == 0):
-                # every other frame
-                x = ring.read_window(mel_width * samplerate, 2 * mel_stride * samplerate) 
+            in_len = len(indata)
+            stride_fractional, stride_whole = math.modf(in_len/mel_stride_bytes)
+            stride_frac_residual += stride_fractional
+
+            for i in range(0,int(stride_whole)):
+                x = audio_ring.read_window(mel_width * rec_sample_rate, i * mel_stride_bytes )
                 mel = calc_mel_for_frame(x)
                 mel_ring.add_mel(mel)
+                run_inferencing()
                 print_ascii_mel(mel)
-
-            x = ring.read_window(mel_width * samplerate, mel_stride * samplerate)
-            mel = calc_mel_for_frame(x)
-            mel_ring.add_mel(mel)
-            print_ascii_mel(mel)
-
-            x = ring.read_window(mel_width * samplerate, 0)
-            mel = calc_mel_for_frame(x)
-            mel_ring.add_mel(mel)
-            print_ascii_mel(mel)
                 
+            if stride_frac_residual >= 1.0:
+                x = audio_ring.read_window(mel_width * rec_sample_rate, int(stride_whole) * mel_stride_bytes)
+                mel = calc_mel_for_frame(x)
+                mel_ring.add_mel(mel)
+                run_inferencing()
+                print_ascii_mel(mel)
+                stride_frac_residual -= 1.0
+
             # Debug info
             frames_log = np.append(frames_log,frames)
         else:
@@ -186,8 +218,8 @@ try:
 
     start_time = time.time()
     with sd.InputStream(device=args.device, channels=1, callback=callback,
-                        blocksize=int(samplerate * args.block_duration / 1000),
-                        samplerate=samplerate):
+                        blocksize=int(rec_sample_rate * args.block_duration / 1000),
+                        samplerate=rec_sample_rate):
         while True:
             response = input()
             if response in ('', 'q', 'Q'):
@@ -207,7 +239,7 @@ except KeyboardInterrupt:
     std = np.std(frames_log)
     print("%i frames mean: %0.2f, std: %0.2f"%(len(frames_log),mean,std))
     print("mel_ring_size: {}, count: {}".format(mel_ring.size, mel_ring.count))
-    ring.save_ring_buffer(samplerate, "audio_ring_buffer.wav")
+    audio_ring.save_ring_buffer(rec_sample_rate, "audio_ring_buffer.wav")
     mel_ring.save_ring_buffer("mel_ring_buffer.txt")
     print("recording lasted {:0.2f} seconds.".format(end_time - start_time))
     parser.exit('Interrupted by user')
